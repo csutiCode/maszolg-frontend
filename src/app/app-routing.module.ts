92c036b6fcd175b25a6b8ca322a950323d1d4873
Gergő <gergo.simon91@gmail.com>
2021-09-12 09:16:20 +0200

    components, first steps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'account', component: AccountComponent }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SearchComponent,
+    HomeComponent,
+    FooterComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,

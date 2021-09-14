cbe4a920a75f7ed9a23ea4dc7e9eec7d7c504b5d
Gergo Simon <gergoe.simon@contractors.twinformatics.at>
2021-09-14 09:47:18 +0200

    table for accounts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 import { ListComponent } from './components/list/list.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegComponent } from './components/reg/reg.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   {path: 'list', component: ListComponent},
   {path: 'login', component: LoginComponent} ,
-  {path: 'reg', component: RegComponent}
+  {path: 'reg', component: RegComponent},
+  {path: 'home', component: HomeComponent}
 
 ];
 

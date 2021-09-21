334d685926b758c66d2c7f8c630c90feb5af96f0
Gergo Simon <gergoe.simon@contractors.twinformatics.at>
2021-09-21 15:11:18 +0200

    myClassifications

--- src/app/components/my-classifications/my-classifications.component.ts
+++ src/app/components/my-classifications/my-classifications.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CookieService } from 'ngx-cookie';
+import { RestService } from 'src/app/services/rest.service';
+import { ListedAccount } from '../listedAccount';
 
 @Component({
   selector: 'app-my-classifications',
@@ -8,12 +13,35 @@ import { Component, OnInit } from '@angular/core';
 export class MyClassificationsComponent implements OnInit {
 
 
+  listedAccount: any;
+
+  uuid: string | null = this.route.snapshot.queryParamMap.get('uuid')
 
 
-
-  constructor() { }
+  constructor( private restService: RestService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getListedAccount(this.uuid);
+
+  }
+
+
+  getListedAccount(uuid: string | null) {
+    return this.restService.get("search/account/" + uuid).subscribe(
+      (data:any)=> {
+        this.listedAccount = data,
+        console.log("Listed Account objekt from the method in MyClassifications Component: ");
+        console.log(this.listedAccount)
+        //I have to call this method here, because all other initialization is too early -> they don't create listedaccount object
+      }
+    )
+  }
+
+
+  reactOnComment() {
+    console.log("React on comment")
+
   }
 
 

d88acba56b72dcabcd10d5cb56a2a2ba98e57211
Gergo Simon <gergoe.simon@contractors.twinformatics.at>
2021-10-11 09:45:23 +0200

    login bug solved

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -34,16 +34,7 @@ export class AuthService {
      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
     "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
  });
-
-
-  reqHeaderAuth =   new HttpHeaders({ 
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + this.cookieService.get("JWT"),
-    'Access-Control-Allow-Credentials': 'true',
-    "Access-Control-Allow-Origin": "http://localhost:8080/*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-  });
+ 
 
   reqHeaderImage =   new HttpHeaders({ 
     'Authorization': 'Bearer ' + this.cookieService.get("JWT"),
@@ -70,38 +61,40 @@ export class AuthService {
     return this.http.post("http://localhost:8080/account/createAccount", registerRequest, { headers: this.reqHeaders }).toPromise();
        
    }
-  
-   public login(loginForm : any) {
-    const promise = this.restService.post("account/login", loginForm.value,  { headers: this.reqHeaderAuth }).toPromise();
 
+ 
+   public login(loginForm : any) {
+    const promise = this.restService.post("account/login", loginForm.value,  { headers: this.getHttpHeaderAuth() }).toPromise();
+    let self = this;
     promise.then((data:any)=> {
           this.token = data,
           console.log(this.token)
-          //set the token as cookie -> works
           this.cookieService.put("JWT", this.token);
           //hide the navbar
           //get the listedAccount and make a redirect
-          this.getListedAccount();
+          self.getListedAccount(self);
       }
     )
   }
+
   
 
-   getListedAccount() {
+   getListedAccount(self : AuthService = this) {
+    
      console.log("TOKEN from the getlistedaccount:  ")
-     console.log(this.token)
-    this.http.get("http://localhost:8080/auth",  { headers: this.reqHeaderAuth }).subscribe(
+     console.log(self.token)
+     self.http.get("http://localhost:8080/auth",  { headers: self.getHttpHeaderAuth() }).subscribe(
       (data:any)=> {
-        this.listedAccount = data,
-        this.router.navigate(['loggedIn'], { queryParams: { uuid: this.listedAccount?.listedAccount_uuid} })
-        this.hide();
+        self.listedAccount = data,
+        self.router.navigate(['loggedIn'], { queryParams: { uuid: self.listedAccount?.listedAccount_uuid} })
+        self.hide();
       }
     ) 
   }
    
 
    public saveListedAccount(form : any) {
-         this.http.post("http://localhost:8080/auth/save/listedAccount", form,  { headers: this.reqHeaderAuth }).subscribe(
+         this.http.post("http://localhost:8080/auth/save/listedAccount", form,  { headers: this.getHttpHeaderAuth() }).subscribe(
           (data:any)=> {
             this.listedAccount = data,
             console.log(this.listedAccount)  
@@ -111,7 +104,7 @@ export class AuthService {
 
    public sendComment(commentOnClassificationDto : any) {
     console.log(commentOnClassificationDto)
-      this.http.post("http://localhost:8080/auth/save/commentOnClassification", commentOnClassificationDto,  { headers: this.reqHeaderAuth }).subscribe(
+      this.http.post("http://localhost:8080/auth/save/commentOnClassification", commentOnClassificationDto,  { headers: this.getHttpHeaderAuth() }).subscribe(
         (data:any)=> {
           this.response = data,
           console.log(this.response)
@@ -125,7 +118,7 @@ export class AuthService {
     }
 
     delete(uuid: string | null) {
-      this.http.get("http://localhost:8080/auth/delete/" + uuid,  { headers: this.reqHeaderAuth }).subscribe(
+      this.http.get("http://localhost:8080/auth/delete/" + uuid,  { headers: this.getHttpHeaderAuth() }).subscribe(
         (data:any)=> {
           this.response = data,
           console.log(this.response)
@@ -144,6 +137,20 @@ export class AuthService {
   show() { this.visible = true; }
 
   toggle() { this.visible = !this.visible; }
+
+
+  getHttpHeaderAuth(): HttpHeaders {
+    const reqHeaderAuth =   new HttpHeaders({ 
+      'Authorization': 'Bearer ' + this.cookieService.get("JWT"),
+      'Content-Type': 'application/json', 
+      'Access-Control-Allow-Credentials': 'true',
+      "Access-Control-Allow-Origin": "http://localhost:8080/*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    });
+
+    return reqHeaderAuth;
+  }
 
 
 
